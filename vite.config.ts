3494bf2044c4e7ee0307aad3078f576e52a3ddad
Darknerior <shotgunsjl2@gmail.com>
2025-07-07 12:02:47 +0100

    Revert "Attempted spa blog post fix"
    This reverts commit 849c18822a87e0dd47603ab7dd5da16b4633cdb0.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,24 +12,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  // This hook runs after the build completes
-  closeBundle: () => {
-    try {
-      fs.copyFileSync("dist/index.html", "dist/404.html");
-      console.log("✔ Copied index.html to 404.html for GitHub Pages SPA support.");
-    } catch (err) {
-      console.error("❌ Failed to copy index.html to 404.html", err);
-    }
-  }
 }));
